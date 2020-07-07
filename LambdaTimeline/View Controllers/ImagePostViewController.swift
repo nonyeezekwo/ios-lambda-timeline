bb5f2689a069f2edd86caa9543788d63a2920d6a
nonye <nnezekwo@gmail.com>
2020-07-07 08:33:30 -0700

    Filter work

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 import Photos
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 class ImagePostViewController: ShiftableViewController {
     
+    enum filterKey: String {
+        case inputImage = "inputImage"
+        case vibrance = "inputAmount"
+        case exposure = "inputEV"
+        case inputCenter = "inputCenter"
+        case distortInputRadius = "inputRadius"
+        case distortInputScale = "inputScale"
+        case kaleidoInputCount = "inputCount"
+        case inputAngle = "inputAngle"
+    }
+    
+    // MARK: - OUTLETS
+//    @IBOutlet weak var imageViewFilter: UIImageView!
+    
+    // MARK: - SLIDER OUTLETS
+    @IBOutlet weak var vibranceSlider: UISlider!
+    @IBOutlet weak var exposureSlider: UISlider!
+    @IBOutlet weak var distortionSlider: UISlider!
+    @IBOutlet weak var kaleidoscopeSlider: UISlider!
+    @IBOutlet weak var hueSlider: UISlider!
+    
+    let vibranceFilter = CIFilter(name: "CIVibrance")
+    let exposureFilter = CIFilter(name: "CIExposureAdjust")
+    let distortionFilter = CIFilter(name: "CIBumpDistortion")
+    let kaleidoscopeFilter = CIFilter(name: "CIKaleidoscope")
+    let hueFilter = CIFilter(name: "CIHueAdjust")
+    
+    private let context = CIContext(options: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        defaultValues()
         setImageViewHeight(with: 1.0)
         
-        updateViews()
+//        updateViews()
+        orginalImage = imageView.image
     }
     
     func updateViews() {
@@ -48,8 +80,66 @@ class ImagePostViewController: ShiftableViewController {
         imagePicker.delegate = self
         
         imagePicker.sourceType = .photoLibrary
-
+        
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //Slider Defaults
+    private func defaultValues() {
+        vibranceSlider.value = 0
+        exposureSlider.value = 0
+        distortionSlider.value = 0.5 // idk
+        kaleidoscopeSlider.value = 0.25 // idk
+        hueSlider.value = 0
+    }
+    
+    private var orginalImage: UIImage? {
+        didSet {
+            guard let image = orginalImage else {
+                scaledImage = nil
+                return
+            }
+            var scaledSize = imageView.bounds.size
+            let scale = UIScreen.main.scale
+            scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height * scale)
+            //imageByScaling is coming from the UIImage+Scaling.swift
+            let scaledImage = image.imageByScaling(toSize: scaledSize)
+            self.scaledImage = scaledImage
+        }
+    }
+    
+    private var scaledImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    //What we call to move slider
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = filterImage(_: scaledImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    // MARK: - ACTIONS FOR SLIDERS
+    @IBAction func vibranceChanged(_ sender: UISlider) {
+        updateImage()
+    }
+    
+    @IBAction func exposureChanged(_ sender: UISlider) {
+        updateImage()
+    }
+    
+    @IBAction func distortionChanged(_ sender: UISlider) {
+        updateImage()
+    }
+    
+    @IBAction func kaleidoscopeChanged(_ sender: UISlider) {
+        updateImage()
+    }
+    
+    @IBAction func hueChanged(_ sender: UISlider) {
+        updateImage()
     }
     
     @IBAction func createPost(_ sender: Any) {
@@ -58,8 +148,8 @@ class ImagePostViewController: ShiftableViewController {
         
         guard let imageData = imageView.image?.jpegData(compressionQuality: 0.1),
             let title = titleTextField.text, title != "" else {
-            presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
-            return
+                presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
+                return
         }
         
         postController.createPost(with: title, ofType: .image, mediaData: imageData, ratio: imageView.image?.ratio) { (success) in
@@ -72,6 +162,94 @@ class ImagePostViewController: ShiftableViewController {
             
             DispatchQueue.main.async {
                 self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
+    private func filterImage(_ image: UIImage) -> UIImage {
+        
+        //UIImage -> CGImage -> CIImage "recipe"
+        
+        guard let cgImage = image.cgImage else { return image }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        let filter = CIFilter.colorControls()
+        
+        //Vibrance Filter
+        vibranceFilter?.setValue(vibranceSlider.value, forKey: filterKey.vibrance.rawValue)
+        guard let vibranceCIImage = vibranceFilter?.outputImage else { return image }
+        
+        //Exposure Filter
+        exposureFilter?.setValue(exposureSlider.value, forKey: filterKey.exposure.rawValue)
+        guard let exposureCIImage = exposureFilter?.outputImage else { return image }
+        
+        //Distortion Filter
+        distortionFilter?.setValue(distortionSlider.value, forKey: filterKey.distortInputScale.rawValue)
+        distortionFilter?.setValue(distortionSlider.value, forKey: filterKey.distortInputRadius.rawValue)
+        distortionFilter?.setValue(CIVector(cgPoint: CGPoint(x: 150, y: 150)), forKey: kCIInputCenterKey)
+//        distortionFilter?.setValue(distortionSlider.value, forKey: filterKey.inputCenter.rawValue)
+        guard let distortionCIImage = distortionFilter?.outputImage else { return image }
+        
+        //Kaleidoscope Filter
+        kaleidoscopeFilter?.setValue(kaleidoscopeSlider.value, forKey: filterKey.kaleidoInputCount.rawValue)
+        kaleidoscopeFilter?.setValue(kaleidoscopeSlider.value, forKey: filterKey.inputCenter.rawValue)
+        kaleidoscopeFilter?.setValue(kaleidoscopeSlider.value, forKey: filterKey.inputAngle.rawValue)
+        guard let kaleidoscopeCIImage = kaleidoscopeFilter?.outputImage else { return image }
+        
+        //Hue Filter
+        hueFilter?.setValue(hueSlider.value, forKey: filterKey.inputAngle.rawValue)
+        guard let hueCIImage = hueFilter?.outputImage else { return image }
+        
+        guard let outputImage = filter.outputImage else {
+            return image
+        }
+        
+        guard let outputCGImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return image
+        }
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    func presentImagePicker() {
+        // Make sure the photo library is available to use in the first place
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            NSLog("The photo library is not available")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //Save button tapped action
+    @IBAction func savePhotoButtonPressed(_ sender: UIButton) {
+        guard let orginalImage = orginalImage else { return }
+        
+        let filteredImage = filterImage(orginalImage)
+        
+        PHPhotoLibrary.requestAuthorization { (status) in
+            
+            guard status == .authorized else {
+                NSLog("The user has not authorized permission for Photo Library Usage")
+                
+                return
+            }
+            PHPhotoLibrary.shared().performChanges({
+                
+                PHAssetCreationRequest.creationRequestForAsset(from: filteredImage)
+                
+            }) { (success, error) in
+                if let error = error {
+                    NSLog("Error saving photo asset: \(error)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    print("Saved photo")
+                }
+                //Present an alert to the user saying that the image was unable to be saved
             }
         }
     }
@@ -128,7 +306,7 @@ class ImagePostViewController: ShiftableViewController {
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
+        
         chooseImageButton.setTitle("", for: [])
         
         picker.dismiss(animated: true, completion: nil)
@@ -138,6 +316,7 @@ extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigation
         imageView.image = image
         
         setImageViewHeight(with: image.ratio)
+        dismiss(animated: false)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
