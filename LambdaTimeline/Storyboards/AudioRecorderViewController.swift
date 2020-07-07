fff2a6ca7341c8e561a6692fc6c0221f4eec185b
nonye <nnezekwo@gmail.com>
2020-07-07 15:08:50 -0700

    Switching to audio branch

---
+++ LambdaTimeline/Storyboards/AudioRecorderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AudioRecorderViewController.swift
+//  LambdaTimeline
+//
+//  Created by Nonye on 7/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecorderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
